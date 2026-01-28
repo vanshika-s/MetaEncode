57c28dcbedc92e16f01877dd3b8e7edb166f6d2f
Zaki-1052 <134018102+Zaki-1052@users.noreply.github.com>
2026-01-27 22:27:17 -0800

    spellcheck

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,4 +3,31 @@
 
 from .cache import CacheManager
 
-__all__ = ["CacheManager"]
+# Lazy imports for optional spell check module
+# (requires symspellpy and jellyfish)
+try:
+    from .spell_check import (
+        VocabularySpellChecker,
+        SpellingSuggestion,
+        get_spell_checker,
+        suggest_correction,
+        correct_spelling,
+    )
+
+    _SPELL_CHECK_AVAILABLE = True
+except ImportError:
+    _SPELL_CHECK_AVAILABLE = False
+    VocabularySpellChecker = None
+    SpellingSuggestion = None
+    get_spell_checker = None
+    suggest_correction = None
+    correct_spelling = None
+
+__all__ = [
+    "CacheManager",
+    "VocabularySpellChecker",
+    "SpellingSuggestion",
+    "get_spell_checker",
+    "suggest_correction",
+    "correct_spelling",
+]
